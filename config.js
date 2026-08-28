ab29baa3860c7295d408bb4f990d06479a42b561
skh27000-ops <skh27000@gmail.com>
2026-08-28 17:53:49 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// ================================================================
+// ImatPath Configuration
+// ================================================================
+
+window.ImatConfig = {
+  // Gemini API Key for the AI Tutor feature.
+  // Get a free key here: https://aistudio.google.com/app/apikey
+  GEMINI_API_KEY: "AQ.Ab8RN6Kiyk1FG0X0TZDhSeLA4ljUiUJ7_WdQQLV75T93QhBc2A",
+  DODO_TEST_API_KEY: "KHZVDqfpoXis3vaC.TOR4VRJju9LzL2Q-mnj7Ct6QO16KHLTTF06rK_hl7mnbZFr2",
+  PAYPAL_CLIENT_ID: "Ab0tQZJ6EUKZH5iZZhyj40RVLn6TXBWlk_tFD0RMB3cheh71ZXowt0iczTBZDROaOzKfRjDumJkVkQyy",
+  
+  // You can add other global configuration variables here in the future
+};
