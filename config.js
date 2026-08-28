51c665194f3c3b3586ae51a784a9b64e6c23838b
skh27000-ops <skh27000@gmail.com>
2026-08-28 20:21:59 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.ImatConfig = {
   // Get a free key here: https://aistudio.google.com/app/apikey
   GEMINI_API_KEY: "AQ.Ab8RN6Kiyk1FG0X0TZDhSeLA4ljUiUJ7_WdQQLV75T93QhBc2A",
   DODO_TEST_API_KEY: "KHZVDqfpoXis3vaC.TOR4VRJju9LzL2Q-mnj7Ct6QO16KHLTTF06rK_hl7mnbZFr2",
-  PAYPAL_CLIENT_ID: "Ab0tQZJ6EUKZH5iZZhyj40RVLn6TXBWlk_tFD0RMB3cheh71ZXowt0iczTBZDROaOzKfRjDumJkVkQyy",
+  PAYPAL_CLIENT_ID: "BAAkzMKQzejwty9Q4_k2_AbKnypLLNHRNTeMiLWZTEArYAC04u9L5RXDMZmNV1VgBNOblwE4mEPLJBQX5l",
   
   // You can add other global configuration variables here in the future
 };
