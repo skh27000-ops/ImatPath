1a53273e30beebf395434671dc8ea7abb2d0d502
skh27000-ops <skh27000@gmail.com>
2026-08-29 17:18:15 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ window.ImatConfig = {
   GEMINI_API_KEY: "AQ.Ab8RN6Kiyk1FG0X0TZDhSeLA4ljUiUJ7_WdQQLV75T93QhBc2A",
   DODO_TEST_API_KEY: "KHZVDqfpoXis3vaC.TOR4VRJju9LzL2Q-mnj7Ct6QO16KHLTTF06rK_hl7mnbZFr2",
   PAYPAL_CLIENT_ID: "BAAkzMKQzejwty9Q4_k2_AbKnypLLNHRNTeMiLWZTEArYAC04u9L5RXDMZmNV1VgBNOblwE4mEPLJBQX5I",
+  TELEGRAM_GROUP_LINK: "https://t.me/+2JUJEOZ6YzNhNzVk",
   
   // You can add other global configuration variables here in the future
 };
